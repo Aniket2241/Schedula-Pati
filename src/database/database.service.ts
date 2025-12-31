cb2cd2aa9a5c337ce366af7ecfe2959fc5168831
Aniket2241 <sprateek507@gmail.com>
2025-12-31 08:14:44 +0530

    fixed db bugs

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,28 +1,31 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
-import {Client} from 'pg';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Client } from 'pg';
+
 @Injectable()
-export class DatabaseService implements OnModuleInit,OnModuleDestroy{
-    private client:Client;
-    constructor(){
-        this.client=new Client({
-            host:process.env.DB_HOST,
-            port:Number(process.env.DB_PORT),
-            user: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-        })
-    }
-async onModuleInit() {
+export class DatabaseService
+  implements OnModuleInit, OnModuleDestroy
+{
+  private client: Client;
+
+  constructor() {
+    this.client = new Client({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+  }
+
+  async onModuleInit() {
     await this.client.connect();
-    console.log('Db connected');
-}
-async onModuleDestroy() {
-    await this.client;
-}
-getClient():Client{
+    console.log('DB connected');
+  }
+
+  async onModuleDestroy() {
+    await this.client.end();
+  }
+
+  getClient(): Client {
     return this.client;
-}
-
-
-
+  }
 }
